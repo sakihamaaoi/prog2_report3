b50a38269c5bca31e4384714c56fbaae00380c4e
Aoi Sakihama <e235723@ie.u-ryukyu.ac.jp>
2023-11-15 00:21:04 +0900

    add .gitignore

--- app/src/main/java/jp/ac/uryukyu/ie/e235723/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235723/Enemy.java
@@ -35,8 +35,14 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
+        if ( hitPoint < 0) {
+            dead = true;
+            damage = 0;
+            hero.wounded(damage);
+        }
+        else hero.wounded(damage);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
+        
     }
 
     /**
